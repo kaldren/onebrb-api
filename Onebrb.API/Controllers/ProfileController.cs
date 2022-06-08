a8a2b00f4bbd4d6abebff01304c99bc02f552cdc
Kaloyan Drenski <drenski666@gmail.com>
2022-06-08 18:44:34 +0300

    Added ProfileController

---
+++ Onebrb.API/Controllers/ProfileController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Onebrb.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+
+    }
+}
