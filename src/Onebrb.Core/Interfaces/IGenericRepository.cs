1cb06f64926b660ae76e2ff83d3e49e455b9de8a
Kaloyan Drenski <drenski666@gmail.com>
2022-06-12 09:19:15 +0300

    Added IGenericRepository

---
+++ src/Onebrb.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Onebrb.Core.Domain;
+
+namespace Onebrb.Core.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        /// <summary>
+        /// Gets a single entity
+        /// </summary>
+        /// <param name="id">Entity identifier</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the entity entry
+        /// </returns>
+        TEntity GetByIdAsync(int id);
+
+        /// <summary>
+        /// Inserts a single entity
+        /// </summary>
+        /// <param name="id">Entity</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// </returns>
+        Task InsertAsync(TEntity entity);
+
+        /// <summary>
+        /// Updates a single entity
+        /// </summary>
+        /// <param name="id">Entity</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// </returns>
+        Task UpdateAsync(TEntity entity);
+
+        /// <summary>
+        /// Deletes a single entity
+        /// </summary>
+        /// <param name="id">Entity</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// </returns>
+        Task DeleteAsync(TEntity entity);
+    }
+}
