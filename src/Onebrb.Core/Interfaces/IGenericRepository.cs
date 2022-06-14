d4e1bc66271e8e7250ac0bc216d0dc27ddafc831
Kaloyan Drenski <drenski666@gmail.com>
2022-06-14 17:30:49 +0300

    IGenericRepository documentation changes.

--- src/Onebrb.Core/Interfaces/IGenericRepository.cs
+++ src/Onebrb.Core/Interfaces/IGenericRepository.cs
@@ -5,31 +5,30 @@ namespace Onebrb.Core.Interfaces
     public interface IGenericRepository<TEntity> where TEntity : BaseEntity
     {
         /// <summary>
-        /// Gets a single entity
+        /// Gets a single entity.
         /// </summary>
         /// <param name="id">Entity identifier</param>
         /// <returns>
-        /// A task that represents the asynchronous operation
-        /// The task result contains the entity entry or null
+        /// Asynchronously returns the <typeparamref name="TEntity" /> entity, or null if no entity is found.
         /// </returns>
         Task<TEntity?> GetByIdAsync(int id);
 
         /// <summary>
-        /// Inserts a single entity
+        /// Inserts a single <typeparamref name="TEntity" /> entity.
         /// </summary>
-        /// <param name="id">Entity</param>
+        /// <param name="entity">The <typeparamref name="TEntity" /> entity to insert.</param>
         void Insert(TEntity entity);
 
         /// <summary>
-        /// Updates a single entity
+        /// Updates a single <typeparamref name="TEntity" /> entity.
         /// </summary>
-        /// <param name="id">Entity</param>
+        /// <param name="entity">The <typeparamref name="TEntity" /> entity to update.</param>
         void Update(TEntity entity);
 
         /// <summary>
         /// Deletes a single entity
         /// </summary>
-        /// <param name="id">Entity</param>
+        /// <param name="entity">The <typeparamref name="TEntity" /> entity to delete.</param>
         void Delete(TEntity entity);
     }
 }
