644c9e736507433c942246c235a495c6de5840a3
Kaloyan Drenski <drenski666@gmail.com>
2022-06-30 15:07:12 +0300

    FromQuery for getting comments by recipientId

--- src/Onebrb.API/Controllers/CommentsController.cs
+++ src/Onebrb.API/Controllers/CommentsController.cs
@@ -8,18 +8,18 @@ namespace Onebrb.API.Controllers
     //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class CommentController : ControllerBase
+    public class CommentsController : ControllerBase
     {
         private readonly ICommentService _commentService;
 
-        public CommentController(ICommentService commentService)
+        public CommentsController(ICommentService commentService)
         {
             _commentService = commentService;
         }
 
-        [HttpGet("{recipientId}")]
+        [HttpGet]
         [ProducesResponseType(200)]
-        public async Task<ActionResult<ICollection<Comment>>> GetCommentsAsync(long recipientId)
+        public async Task<ActionResult<ICollection<Comment>>> GetCommentsAsync([FromQuery] long recipientId)
         {
             var comments = await _commentService.GetCommentsAsync(recipientId);
 
