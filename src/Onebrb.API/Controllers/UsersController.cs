f45790e37d8bddaac038e854c65602677495720a
Kaloyan Drenski <drenski666@gmail.com>
2022-07-20 16:07:38 +0300

    Fixes

--- src/Onebrb.API/Controllers/UsersController.cs
+++ src/Onebrb.API/Controllers/UsersController.cs
@@ -44,24 +44,18 @@ namespace Onebrb.API.Controllers
         [HttpGet("current")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<ActionResult<Domain.Entities.Profile.Profile>> GetCurrentlyAuthenticatedUserAsync()
+        public async Task<ActionResult<UserProfileModel>> GetCurrentlyAuthenticatedUserAsync()
         {
             string? currentUserId = User?.Claims?.FirstOrDefault(x => x.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier")?.Value;
 
             if (currentUserId is null) return NotFound();
 
-            var res = await _mediator.Send(new GetUserProfileByIdQuery() { Id = currentUserId });
+            var user = await _mediator.Send(new GetUserProfileByIdQuery() { Id = currentUserId });
 
-            //if (currentUserEmail is null)
-            //    return Unauthorized();
+            if (user is null)
+                return NotFound();
 
-            //var profile = await _profileService.GetProfileAsync(currentUserEmail);
-
-            //if (profile == null)
-            //    return NotFound();
-
-            //return Ok(profile);
-            return null;
+            return Ok(user);
         }
 
         [HttpGet]
