fa250818440aaeed3efd2de1029e12fd8ccea81e
Kaloyan Drenski <drenski666@gmail.com>
2022-07-20 15:03:00 +0300

    Removed Activate command

--- src/Onebrb.API/Controllers/UsersController.cs
+++ src/Onebrb.API/Controllers/UsersController.cs
@@ -2,7 +2,6 @@
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Onebrb.API.Models;
 using Onebrb.Application.Comments.Models;
 using Onebrb.Application.Users.Models;
 using Onebrb.Application.Users.Queries.GetAllCommentsByUserId;
@@ -53,63 +52,9 @@ namespace Onebrb.API.Controllers
             return Ok(res);
         }
 
-        [HttpGet("current")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<ActionResult<Domain.Entities.Profile.Profile>> GetCurrentlyAuthenticatedUserAsync()
-        {
-            //string? currentUserEmail = User?.Claims?.FirstOrDefault(x => x.Type == "emails")?.Value;
-
-            //if (currentUserEmail is null)
-            //    return Unauthorized();
-
-            //var profile = await _profileService.GetProfileAsync(currentUserEmail);
-
-            //if (profile == null)
-            //    return NotFound();
-
-            //return Ok(profile);
-            return null;
-        }
-
-        [HttpPost("profiles/activate")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<ActionResult<ActivatedUserProfileResponseModel>> ActivateUserProfileAsync()
-        {
-            string? currentUserId = User?.Claims?.FirstOrDefault(x => x.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier")?.Value;
-            string? currentUserEmail = User?.Claims?.FirstOrDefault(x => x.Type == "emails")?.Value;
-            string? currentUserName = User?.Claims?.FirstOrDefault(x => x.Type == "name")?.Value;
-
-            UserProfileModel profile = await _mediator.Send(new Application.Users.Commands.ActivateUserProfileCommand()
-            {
-                ProfileModel = new ActivateUserProfileModel
-                {
-                    Id = currentUserId,
-                    Email = currentUserEmail,
-                    Name = currentUserName
-                }
-            });
-
-            if (profile == null) return NotFound();
-
-            return Ok(_mapper.Map<ActivatedUserProfileResponseModel>(profile));
-        }
-
         [HttpPatch]
         public async Task<ActionResult> EditProfileAsync([FromBody][Required] Domain.Entities.Profile.Profile profileModel)
         {
-            //string? currentUserEmail = User?.Claims?.FirstOrDefault(x => x.Type == "emails")?.Value;
-
-            //if (currentUserEmail is null || profileModel?.Email != currentUserEmail)
-            //    return Unauthorized();
-
-            //var profile = await _profileService.UpdateProfileAsync(profileModel);
-
-            //if (profile == null)
-            //    return NotFound();
-
-            //return Ok(profile);
             return null;
         }
     }
